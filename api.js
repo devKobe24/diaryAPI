a6dba515f5413a3b78d12654a2e19e629a467c80
kobe <dev.skyachieve91@gmail.com>
2024-02-01 15:42:40 +0900

    :green_heart: Chore: fix access level and ip.

--- api.js
+++ api.js
@@ -15,7 +15,7 @@ const express = require('express');
 const app = express();
 
 /* 포트 설정 */
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000, '0.0.0.0');
 
 /* 공통 미들웨어 */
 app.use(morgan('dev'));
