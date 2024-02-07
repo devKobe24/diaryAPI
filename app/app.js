3cea55754978d58d30b91987a0cacac14d1d7da2
kobe <dev.skyachieve91@gmail.com>
2024-02-08 07:43:55 +0900

    :sparkles: Feat: make front-end part javascript file.

--- app/app.js
+++ app/app.js
@@ -9,6 +9,9 @@ const home = require("./src/routes/home");
 // 앱 세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
-app.use("/", home); // use() -> 미들웨어를 등록하는 메서드
+
+// 미들 웨어
+app.use(express.static(`${__dirname}/src/public`)); // express.static() -> 정적 경로 추가하는 메서드
+app.use("/", home); // use() -> 미들 웨어를 등록하는 메서드
 
 module.exports = app;
