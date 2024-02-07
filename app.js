d13928e5252eb7ebf37ab3bb244b34f2655e3bdc
kobe <dev.skyachieve91@gmail.com>
2024-02-08 06:32:51 +0900

    :sparkles: Chore: app.lister() 모듈화.

--- app.js
+++ app.js
@@ -3,8 +3,6 @@
 const express = require("express");
 const app = express();
 
-const PORT = 3000;
-
 // 라우팅
 const home = require("./routes/home");
 
@@ -12,6 +10,4 @@ const home = require("./routes/home");
 app.set("view engine", "ejs");
 app.use("/", home); // use() -> 미들웨어를 등록하는 메서드
 
-app.listen(PORT, function() {
-  console.log("서버 가동");
-});
+module.exports = app;
